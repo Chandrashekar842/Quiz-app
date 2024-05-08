23e64776fe7537c76a042c4e348f34074d1497df
Chandra Shekar <mchandrashekar842@gmail.com>
2024-05-08 13:08:23 +0530

    final screen and score

--- src/pages/FinalScreen.jsx
+++ src/pages/FinalScreen.jsx
@@ -1,9 +1,25 @@
+import { Box, Button, Typography } from '@mui/material'
 import React from 'react'
+import { useDispatch, useSelector } from 'react-redux'
+import { useNavigate } from 'react-router-dom'
+import { changeScore , changeQuestionCount} from '../redux/reducer'
 
 export const Finalscreen = () => {
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
+  const { score } = useSelector(state => state.selectionChoices)
+
+  const handleClick = () => {
+    dispatch(changeScore(0))
+    dispatch(changeQuestionCount(50))
+    navigate('/')
+  }
+
   return (
-    <div>
-      Final score
-    </div>
+    <Box mt={30} >
+      <Typography variant='h3' fontWeight='bold' mb={3}>Final Score: {score}</Typography>
+      <Button onClick={handleClick}>Back to Settings</Button>
+    </Box>
   )
 }
--- src/pages/Questions.jsx
+++ src/pages/Questions.jsx
@@ -1,7 +1,10 @@
 import { Box, Button, CircularProgress, Typography } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { useAxios } from "../hooks/useAxios";
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
+import { useNavigate } from "react-router-dom";
+import { changeScore } from "../redux/reducer";
+import { decode } from "html-entities";
 
 const getRandomInt = (max) => {
   return Math.floor(Math.random() * Math.floor(max));
@@ -13,7 +16,11 @@ export const Questions = () => {
     question_difficulty,
     question_type,
     question_count,
+    score
   } = useSelector((state) => state.selectionChoices);
+
+  const navigate = useNavigate()
+  const dispatch = useDispatch()
 
   let apiUrl = `/api.php?amount=${question_count}`;
 
@@ -51,18 +58,31 @@ export const Questions = () => {
     );
   }
 
+  const handleAnswerClick = (e) => {
+    const question = response?.data.results[questionIndex]
+    if(question.correct_answer === e.target.textContent) {
+      dispatch(changeScore(score + 1))
+
+    } 
+    if(questionIndex + 1 < response.data.results.length) {
+      setQuestionIndex(questionIndex+1)
+    } else {
+      navigate('/score')
+    }
+  }
+
   return (
     <Box>
       <Typography variant="h4">Question {questionIndex + 1}</Typography>
       <Typography mt={5}>
-        {response.data.results[questionIndex].question}
+         {decode(response.data.results[questionIndex].question)}
       </Typography>
       {options.map((data, id) => (
-        <Box mt={2} key={id}>
-          <Button variant="contained">{data}</Button>
+        <Box mt={2} key={id} className='ans'>
+          <Button onClick={handleAnswerClick} variant="contained">{decode(data)}</Button>
         </Box>
       ))}
-      <Box mt={5}>Score: 2/5</Box>
+      <Box mt={5}>Score: {score}/ {response?.data.results.length}</Box>
     </Box>
   );
 };
